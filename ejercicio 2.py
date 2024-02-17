43a7febb0715d5a6075b7b4744a330851be6f479
Daniel calle <danielucoarango@gmail.com>
2024-02-16 20:34:20 -0500

    initil commit add ejercicio 1.py add ejercicio 2.py

---
+++ ejercicio 2.py
@@ -0,0 +1,13 @@
+import math
+
+def calcular_area_circulo(radio):
+
+    area = math.pi * radio**2
+    return area
+
+
+radio = float(input("Ingrese el radio del círculo: "))
+
+
+area = calcular_area_circulo(radio)
+print("El área del círculo con radio", radio, "es:", area)
